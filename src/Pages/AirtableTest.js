83a750f1c4d4b5c9781cc8e3d133acb5f659be34
Christopher Gregorian <csgregorian@gmail.com>
2021-01-28 10:22:38 -0400

    Add typechecking

--- src/Pages/AirtableTest.js
+++ src/Pages/AirtableTest.js
@@ -1,21 +1,27 @@
+// @flow
+
 import Airtable from "airtable";
+import React from 'react';
 import { useEffect, useState } from "react";
-const API_KEY = process.env.REACT_APP_AIRTABLE_KEY;
+const API_KEY: string = process.env.REACT_APP_AIRTABLE_KEY || "";
 
-export const AirtableTest = () => {
-    const [records, setRecords] = useState([]);
+export const AirtableTest = (): React$Element<"div"> => {
+  const [records: Array< { [key: string]: any } >, setRecords: (Array<{ [key: string]: any }>) => void] = useState([]);
 
-    useEffect(() => {
-        var base = new Airtable({ apiKey: API_KEY }).base('appV6oQxcgRYsx3Wf');
+useEffect(() => {
+  var base = new Airtable({ apiKey: API_KEY }).base('appV6oQxcgRYsx3Wf');
 
-        base('Recipes').select().all().then(result => {
-            setRecords(result);
-        })
-    }, [])
-    return <div>
-        <h1>{API_KEY}</h1>
-        <pre>{JSON.stringify(records, null, 2)}</pre>
-    </div>
+  base('Recipes')
+    .select()
+    .all()
+    .then((result: Array<{ [key: string]: any }>): void => {
+      setRecords(result);
+    })
+}, [])
+return <div>
+  <h1>{API_KEY}</h1>
+  <pre>{JSON.stringify(records, null, 2)}</pre>
+</div>
 }
 
 export default AirtableTest;
