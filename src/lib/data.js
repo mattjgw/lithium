e35e4aec966159ada8c5f4be50646351aedcc9f1
Christopher Gregorian <csgregorian@gmail.com>
2021-02-04 13:16:52 -0400

    Add starting data

---
+++ src/lib/data.js
@@ -0,0 +1,42 @@
+export const device_names = ["microwave", "refrigerator", "stove", "oven", "tv",
+  "air conditioner", "hair dryer", "dishwasher", "clothes washer", "clothes dryer"]
+
+export const device_daily_freq = {
+  "microwave": 5,
+  "refrigerator": 40,
+  "stove": 1,
+  "oven": 0.45,
+  "tv": 1.62,
+  "air conditioner": 1.5,
+  "hair dryer": 1.5,
+  "dishwasher": 1,
+  "clothes washer": 0.36,
+  "clothes dryer": 0.25,
+}
+
+export const device_cycle_length = {
+  "microwave": 3,
+  "refrigerator": 12,
+  "stove": 12,
+  "oven": 30,
+  "tv": 90,
+  "air conditioner": 120,
+  "hair dryer": 7,
+  "dishwasher": 75,
+  "clothes washer": 55,
+  "clothes dryer": 75,
+}
+
+export const device_wattage = {
+  "microwave": 1500,
+  "refrigerator": 110,
+  "stove": 2000,
+  "oven": 3000,
+  "tv": 105,
+  "air conditioner": 1300,
+  "hair dryer": 1600,
+  "dishwasher": 1140,
+  "clothes washer": 1200,
+  "clothes dryer": 5000,
+}
+
