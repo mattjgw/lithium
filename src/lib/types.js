4f02444b47e28360c10814b16a33ef794fb2708e
Christopher Gregorian <csgregorian@gmail.com>
2021-02-06 15:39:40 -0400

    Add questionnaire type

---
+++ src/lib/types.js
@@ -0,0 +1,23 @@
+// @flow
+
+export type QuestionnaireResponse = {
+  location: string,
+  squareFootage: string,
+  monthlyUsage: string,
+  dishwasher: boolean,
+  stove: boolean,
+  oven: boolean,
+  fridge: boolean,
+  freezer: boolean,
+  washerDryer: boolean,
+  heat: boolean,
+  ac: boolean,
+  dishwasherUsage: string,
+  stoveUsage: string,
+  ovenUsage: string,
+  secondFridge: string,
+  secondFreezer: string,
+  washerUsage: string,
+  dryerUsage: string,
+  acUsage: string,
+}
