5a819e16727cdb1f5f0ce6a8d6d5eeeb81cbf044
Christopher Gregorian <csgregorian@gmail.com>
2021-02-06 16:10:03 -0400

    Add explained questionnaire data fields

--- src/lib/types.js
+++ src/lib/types.js
@@ -2,8 +2,16 @@
 
 export type QuestionnaireResponse = {
   location: string,
-  squareFootage: string,
-  monthlyUsage: string,
+  // used to identify outage patterns
+
+  squareFootage: number,
+  // square feet of house: used to estimate air conditioning
+  // and heating usage (cycle length and frequency)
+
+  monthlySummerUsage: number, // kWh per month
+  monthlyWinterUsage: number, // kWh per month
+  // used to scale 
+
   dishwasher: boolean,
   stove: boolean,
   oven: boolean,
@@ -12,12 +20,20 @@ export type QuestionnaireResponse = {
   washerDryer: boolean,
   heat: boolean,
   ac: boolean,
-  dishwasherUsage: string,
-  stoveUsage: string,
-  ovenUsage: string,
-  secondFridge: string,
-  secondFreezer: string,
-  washerUsage: string,
-  dryerUsage: string,
-  acUsage: string,
+  secondFridge: boolean,
+  secondFreezer: boolean,
+  // indicates whether the user owns each device,
+  // leads to more detailed questions about usage
+
+  dishwasherUsage: number,
+  stoveUsage: number,
+  ovenUsage: number,
+  washerUsage: number,
+  dryerUsage: number,
+  // estimated cycles per week
+  // used to scale the hourly probability of each cycle
+
+  acUsage: number,
+  // estimated days of air conditioning per month
+  // in the summer
 }
